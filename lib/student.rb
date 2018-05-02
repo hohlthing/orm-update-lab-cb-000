a913585620c17018de815edd8a4c6ff7049297ee
michael hohl <mhohl@rocketmail.com>
2018-05-02 19:56:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,4 +14,59 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def self.create(name, grade)
+  end
+
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
+  def self.find_by_name(name)
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+  SQL
+
+  DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
+
+  def updates
+  end
+
+end
+
+
 end

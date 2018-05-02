e82ca078b2ad3bdd0cd65530ea6d4bea3b68656a
michael hohl <mhohl@rocketmail.com>
2018-05-02 20:03:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def initialize(name, grade, id = nil)
+  def initialize(id = nil, name, grade)
     @id = id
     @name = name
     @grade = grade
@@ -20,8 +20,8 @@ class Student
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade TEXT
-    )
-    SQL
+      )
+      SQL
 
     DB[:conn].execute(sql)
   end
@@ -35,7 +35,7 @@ class Student
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
-    SQL
+      SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
@@ -55,12 +55,12 @@ class Student
   end
 
   def self.find_by_name(name)
-  sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE name = ?
-    LIMIT 1
-  SQL
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
 
   DB[:conn].execute(sql, name).map do |row|
     self.new_from_db(row)
